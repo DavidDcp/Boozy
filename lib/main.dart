c756f1633d5f0c28deb39cae151fde9a8d56c225
DAVID <ddelcamp@ucm.es>
2023-10-12 17:08:41 -0500

    UsuariosScreen iniciada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:boozy/presentation/screens/home_screen.dart';
+import 'package:boozy/presentation/screens/usuarios_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const UsuariosInicio(),
     );
   }
 }
---
+++ lib/presentation/screens/usuarios_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class UsuariosInicio extends StatelessWidget {
+  const UsuariosInicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: UserBody(),
+    );
+  }
+}
+
+class UserBody extends StatelessWidget {
+  const UserBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const List<IconData> items = [Icons.people, Icons.replay, Icons.list_alt];
+    // TODO: implement build
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('/images/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: items.map((i) =>
+                IconButton(onPressed: () {}, icon: Icon(i))).toList(),
+          ),
+          const Text("JUGADORES"),
+          Container(
+            child: const Text("LIsta de jugadores"),
+            height: 300,
+            //width: 300,
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text("Empezar")),
+
+        ],
+      ),
+    );
+  }
+}
