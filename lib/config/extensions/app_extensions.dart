9371bc76d40e65332e466816e793d83452223351
DAVID <ddelcamp@ucm.es>
2023-11-08 10:32:45 -0500

    Cambios para correcto funcionamiento en movil

--- lib/config/extensions/app_extensions.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:math';
-
-extension AngleConvertions on num {
-  double toRad() => this * pi / 180;
-
-  double toDeg() => this * 180 / pi;
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
-import 'package:boozy/presentation/screens/home_screen.dart';
-import 'package:boozy/presentation/screens/reglas_screen.dart';
-import 'package:boozy/presentation/screens/usuarios_screen.dart';
+import 'package:boozy/config/router/app_router.dart';
+import 'package:boozy/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  const ProviderScope(
+    child: MyApp(),
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      title: 'Boozy Game',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const ReglasScreen(),   //UsuariosInicio  HomeScreen
+      theme: AppTheme().getTheme(),
     );
   }
 }
